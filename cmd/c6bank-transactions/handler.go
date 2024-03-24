e58950a84e646b325aa6f2e62acf7cca73c51991
Danilo Sousa <code@danilosousa.net>
2024-03-24 21:26:47 +0000

    Refactor handler to output QIF file

--- cmd/c6bank-transactions/handler.go
+++ cmd/c6bank-transactions/handler.go
@@ -11,7 +11,10 @@ import (
 	"strings"
 
 	"git.home/c6bank-transactions/internal/parser"
-	"git.home/c6bank-transactions/internal/qif"
+)
+
+const (
+	qifMIME = "text/qif"
 )
 
 //go:embed index.html
@@ -44,6 +47,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "missing `file` param", http.StatusBadRequest)
 		return
 	}
+	file := files[0]
 
 	number, ok := r.MultipartForm.Value["number"]
 	if !ok || len(number) > 1 {
@@ -51,70 +55,55 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fileHeader := files[0]
-	log.Printf("received upload file %s of size %d\n", fileHeader.Filename, fileHeader.Size)
-
-	if fileHeader.Size > MAX_UPLOAD_SIZE {
-		http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 10MB in size", fileHeader.Filename), http.StatusBadRequest)
+	if file.Size > MAX_UPLOAD_SIZE {
+		http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 10MB in size", file.Filename), http.StatusBadRequest)
 		return
 	}
 
-	file, err := fileHeader.Open()
+	body, err := file.Open()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer file.Close()
+	defer body.Close()
 
-	if err = validateUploadFile(fileHeader.Filename, file); err != nil {
+	filetype, err := validateUploadFile(file.Filename, body)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	csv, err := qif.ParseCSV(file)
+	output, err := parser.Parse(file.Filename, body, file.Size, number[0])
 	if err != nil {
-		http.Error(w, fmt.Sprintf("could not parse %s: %s", fileHeader.Filename, err), http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("could not parse %s: %s", file.Filename, err), http.StatusBadRequest)
 		return
 	}
 
-	// csv, err := parser.Parse(fileHeader.Filename, file, number[0])
-	// if err != nil {
-	// 	http.Error(w, fmt.Sprintf("could not parse %s: %s", fileHeader.Filename, err), http.StatusBadRequest)
-	// 	return
-	// }
+	filename := strings.TrimSuffix(file.Filename, filepath.Ext(file.Filename)) + ".qif"
+	log.Printf("INFO received upload %s of type %s and parsed as %s\n", file.Filename, filetype, filename)
 
-	out, err := io.ReadAll(csv)
+	w.Header().Set("Content-Type", qifMIME)
+	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;filename="%s"`, filename))
+
+	_, err = io.Copy(w, output)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("cloud not read csv: %s", err), http.StatusBadRequest)
-		return
-	}
-
-	csvFilename := strings.TrimSuffix(fileHeader.Filename, filepath.Ext(fileHeader.Filename))
-	log.Printf("upload parsed as %s.csv: %d\n", csvFilename, len(out))
-
-	w.Header().Set("Content-Type", "text/csv")
-	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;filename="%s.csv"`, csvFilename))
-
-	_, err = w.Write(out)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("cloud not write csv response: %s", err), http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("cloud not write response: %s", err), http.StatusBadRequest)
 		return
 	}
 }
 
-func validateUploadFile(name string, file multipart.File) error {
+func validateUploadFile(name string, file multipart.File) (string, error) {
 	buff := make([]byte, 512)
 	if _, err := file.Read(buff); err != nil {
-		return err
+		return "", err
 	}
 
 	filetype := http.DetectContentType(buff)
-	log.Printf("received upload %s of type %s\n", name, filetype)
 
 	if err := parser.IsValid(name, filetype); err != nil {
-		return err
+		return "", err
 	}
 
 	_, err := file.Seek(0, io.SeekStart)
-	return err
+	return filetype, err
 }
