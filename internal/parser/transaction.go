e11f46fe888810cdc1beee0d9b50859978c65085
Danilo Sousa <code@danilosousa.net>
2025-02-20 21:47:57 -0300

    Create `Transaction` struct

---
+++ internal/parser/transaction.go
@@ -0,0 +1,56 @@
+package parser
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Transaction struct {
+	Date        time.Time
+	Payee       string
+	Memo        string
+	Amount      string
+	Installment bool
+	Future      bool
+}
+
+func (t *Transaction) ParseDate(ct CurrentTime, date string) error {
+	now := ct.Now()
+	year := now.Year()
+
+	month, err := strconv.Atoi(date[3:5])
+	if err != nil {
+		return err
+	}
+
+	day, err := strconv.Atoi(date[:2])
+	if err != nil {
+		return err
+	}
+
+	if t.Installment {
+		parts := strings.SplitN(t.Memo, "/", 2)
+
+		if parts[0] != "1" {
+			installment, err := strconv.Atoi(parts[0])
+			if err != nil {
+				return err
+			}
+
+			month = (month + installment) % 12
+		}
+	} else {
+		if month > int(now.Month()) {
+			year = now.AddDate(-1, 0, 0).Year()
+		}
+	}
+
+	t.Date = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+
+	return nil
+}
+
+func (ts Transaction) CSVLine() []string {
+	return []string{ts.Date.Format(dateFormat), ts.Payee, ts.Memo, ts.Amount}
+}
---
+++ internal/parser/transaction_test.go
@@ -0,0 +1,58 @@
+package parser_test
+
+import (
+	"testing"
+	"time"
+
+	"git.home/c6bank-transactions/internal/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+// mockTime: 1985-08-05
+
+func TestTransaction_ParseDate(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		date        string
+		expected    string
+		transaction parser.Transaction
+	}{
+		{name: "current year, before now", date: "21/03", expected: "1985-03-21"},
+		{name: "later same year", date: "21/09", expected: "1984-09-21"},
+		{
+			name: "first installment", date: "21/08", expected: "1985-08-21",
+			transaction: parser.Transaction{Installment: true, Memo: "1/4 4321 01/1985"},
+		},
+		{
+			name: "old installment", date: "21/05", expected: "1985-08-21",
+			transaction: parser.Transaction{Installment: true, Memo: "3/4 4321 01/1985"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			tx := test.transaction
+
+			assert.NoError(t, tx.ParseDate(mockTime, test.date))
+			assert.Equal(t, test.expected, tx.Date.Format(time.DateOnly))
+		})
+	}
+}
+
+func TestTransaction_CSVLine(t *testing.T) {
+	t.Parallel()
+
+	ts := parser.Transaction{
+		Date:   time.Date(1985, time.December, 26, 0, 0, 0, 0, time.UTC),
+		Payee:  "Payee",
+		Memo:   "Memo",
+		Amount: "123.45",
+	}
+
+	csv := ts.CSVLine()
+	assert.Equal(t, []string{"26/12/1985", "Payee", "Memo", "123.45"}, csv)
+}
